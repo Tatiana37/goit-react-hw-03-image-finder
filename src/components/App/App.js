8619f681c50c5fcb4305d1f0b850d574b37fe631
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-10-12 00:03:32 +0300

    Initial files

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+// import { Component } from 'react';
+// import Form from '../Form/Form';
+// import FilterSearch from '../FilterSearch/FilterSearch';
+// import ContactList from '../ContactList/ContactList';
+import Container from '../Container/Container';
+// import data from '../../contacts.json';
+
+function App(){
+  return (
+    <div>goit-react-hw-03-image-finder
+    <Container/>
+    </div>
+  )
+}
+
+export default App;
